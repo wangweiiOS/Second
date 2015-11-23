6882293fafa4bdaadb1d6f0ab664b5c546e99df4
David <david@DaviddeMacBook-Pro.local>
2015-11-23 14:56:40 +0800

    111

---
+++ lululu/lululu/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  lululu
+//
+//  Created by David on 15/11/23.
+//  Copyright (c) 2015年 王伟. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor=  [UIColor whiteColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
